d53a4e7bd8a9da9503c9953db02c33459766f496
Test Account <testaccount@Tests-Macintosh-2.local>
2015-07-06 15:38:39 -0700

    Initial Commit

---
+++ ContainingAppForBard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainingAppForBard
+//
+//  Created by Test Account on 7/6/15.
+//  Copyright (c) 2015 Vibin Kundukulam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
